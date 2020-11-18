26374ec52c43896e19618e9a520bc55bc10fdeaf
Vaibhav <mailvaibhavchopra@gmail.com>
2020-11-18 13:43:49 +0530

    Initial Commit

---
+++ Hacker-News-SwiftUI-iOS/Hacker_News_SwiftUI_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News_SwiftUI_iOSApp.swift
+//  Hacker-News-SwiftUI-iOS
+//
+//  Created by Vaibhav Chopra on 18/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News_SwiftUI_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
